05059eb09b2db29562aa89dd3eb08d48cb19ca65
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-10-30 21:28:46 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,50 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+              int i,x,n,p,max,min;
+              i=0;
+              x=0;
+              n=0;
+              p=0;
+              do
+              { 
+                 cout<<"inserisci un numero"<<endl;
+                 cin>>n;
+                 if(i=0)
+                 {
+                    max=n;
+                    }
+                elseif(n>max)
+                {
+                    max=n;
+                    }
+              if(i=0)
+              {
+                min=n;
+               }
+                    elseif(n<min)
+                    {  min=n;
+                    }
+
+                if(n>=0)
+                {  
+                    p=p+n;
+                    x=x+1;
+                    else
+                    {
+                        n=n+1;
+                        }
+                        while(n!=0)
+        }
+        {
+            cout<<"il valore massimo è" <<max <<endl;
+            cout<<"il valore minimo è" <<min <<endl;
+            cout<<"ho inserito" << n << " valori negativi" <<endl;
+            cout<<"la media dei valori positivi è" << (p/x) <<endl;
+            }
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
